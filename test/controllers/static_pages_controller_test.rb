9c1c7a465460263d720f231f583f04ce3134c769
Mac Shu <mac_shu11@hotmail.com>
2015-06-19 03:44:24 +0000

    Improve the Static Page Controller, Added MiniTest Reporters and Backtrace silencer and Guard

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,28 +1,32 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  def setup
+    @base_title = "CarServIt"
+  end
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | CarServIt"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | CarServIt"
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact Us | CarServIt"
+    assert_select "title", "Contact Us | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | CarServIt"
+    assert_select "title", "Help | #{@base_title}"
   end
 
 end
