27eb508024a60a1188a955d03834ed371f7880ca
Mac Shu <mac_shu11@hotmail.com>
2015-06-19 04:22:48 +0000

    Complete Layout (Without CSS)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # Return the full title as a per-page basics
+  def full_title(page_title = '')
+    base_title = "CarServIt"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  get 'static_pages/help'
+  root              'static_pages#home'
+  get 'about'   =>  'static_pages#about'
+  get 'contact' =>  'static_pages#contact'
+  get 'help'    =>  'static_pages#help'
 
  end
