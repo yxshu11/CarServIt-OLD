cdc1cd51c39ec98a8f1f960e5149953aa5d3069d
Mac Shu <mac_shu11@hotmail.com>
2015-06-19 03:31:44 +0000

    Finish Static Pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,21 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | CarServIt"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | CarServIt"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact Us | CarServIt"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | CarServIt"
   end
 
 end
